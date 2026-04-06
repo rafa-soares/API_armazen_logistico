e756499dbddbc7ba1f8b4327b93e9bce8f709713
rafaelasoaresdesa <rafa.soaresdesa@gmail.com>
2026-04-06 09:00:34 -0300

    add documentation swagger.json

--- src/test/java/com/wms/inbound/entrypoint/controller/SellerControllerTest.java
+++ src/test/java/com/wms/inbound/entrypoint/controller/SellerControllerTest.java
@@ -13,22 +13,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 class SellerControllerTest {
     @Autowired
     private SellerRepository sellerRepository;
 
     @Autowired
     private TestRestTemplate template;
-
-    @AfterAll
-    void init() {
-        sellerRepository.deleteAll();
-    }
 
     @Test
     public void shouldCreateSeller() {
